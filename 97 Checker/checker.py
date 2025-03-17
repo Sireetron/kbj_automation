fe16ef2931e9185243b2d84764e2611e774a05f5
Sireetron Lertsuriyakul <Sireetron@kbjcapital.co.th>
2025-03-17 19:03:38 +0700

    add

--- 97 Checker/checker.py
+++ 97 Checker/checker.py
@@ -1,57 +1,126 @@
+import os
 import pandas as pd
-# from utils import all_history, transform_files
+import glob
+import pandas as pd
 import glob
 import numpy as np
 import pandas as pd
 import glob
 # import win32com.client as win32
 import jaydebeapi
-# from const import QUERY, CONNECT
 import re
 
-assign = pd.read_excel(f"./input/3.Total assign Mar'25 (1).xlsx", dtype={'Mobile No.': str, 
-                                                        })
-assign_delay = assign.loc[
-    (assign['New OA'] == 'Delay calling')
-][['Base date','Customer ID No','Principal balance','Customer Name', 'Sales product code','Mobile No.'
-        ,'Overdue Months Division Code','Loan No.','Overdue days(Morning)','Status LM','New OA' ]]
 
-assign_delay['Customer Name'] = assign_delay['Customer Name'].str.strip()
-assign_delay[['NAME', 'SURNAME']] = assign_delay['Customer Name'].str.split(n=1, expand=True)
+# Define the input folder
+input_folder = "./input/assign_input/"
 
 
-customer = pd.read_csv('./input/1stday/customerdata.csv', dtype={'MOBILE_PHONE_NO_VAL': str, 
-                                                           'NATIONAL_ID_VAL': str, 
-                                                           'CONTRACT_NO_VAL': str})
+# Get a list of files in the folder (xlsx or csv)
+files = glob.glob(os.path.join(input_folder, "*.xlsx")) + glob.glob(os.path.join(input_folder, "*.csv"))
+print(files)
 
-assign_delay['Customer ID No'] = assign_delay['Customer ID No'].astype(str)
-assign_delay['Loan No.'] = assign_delay['Loan No.'].astype(str)
-customer['CONTRACT_NO_VAL'] = customer['CONTRACT_NO_VAL'].astype(str)
-customer['NATIONAL_ID'] = customer['NATIONAL_ID'].astype(str) 
 
-assign_delay_merge = assign_delay.merge(customer, left_on=['Customer ID No', 'Loan No.'], right_on=['NATIONAL_ID' ,'CONTRACT_NO_VAL'], how='left')
-assign_delay_merge['NAME_CHECK'] = assign_delay_merge.apply(
-    lambda row: True if row['NAME'] == row['NAME_VAL'] else row['NAME_VAL'],
+# Read only the first file found
+if files:
+    file_path = files[0]
+    # print(f"Reading file: {file_path}")
+    
+    if file_path.endswith(".xlsx"):
+        assign = pd.read_excel(file_path,sheet_name='Data SMS', dtype={'MobileNo': str})
+    elif file_path.endswith(".csv"):
+        assign = pd.read_csv(file_path,sheet_name='Data SMS', dtype={'MobileNo': str})
+    
+    # print(assign.head())  # Display first few rows
+else:
+    print("No file found in the folder.")
+    
+
+def clean_column_names(df):
+    df.columns = df.columns.str.lower()  # Convert to lowercase
+    df.columns = df.columns.str.replace(r'\.', '', regex=True)
+    df.columns = df.columns.str.replace(r' ', '_', regex=True)  # Replace non-alphanumeric characters with underscores
+    df.columns = df.columns.str.replace('customer_id_no|customer_no|customer_id', 'customer_no', regex=True)  # Replace variations with 'customer_no'
+    df.columns = df.columns.str.replace(r'loan|loan_no|contract.*', 'contract_no', regex=True)
+    df.columns = df.columns.str.replace(r'mobile.*', 'mobile_no', regex=True)
+    df.columns = df.columns.str.replace(r'customer_name/surname\(thai\)', 'customer_name', regex=True)  # Replace 'customer_name/surname(thai)' with 'customer_name'
+    return df
+
+assign_delay = clean_column_names(assign)
+
+
+customer = pd.read_csv('./input/customer_input/customerdata.csv', dtype={'mobile_phone_no_val': str, 
+                                                           'natioanal_id_val': str, 
+                                                           'contract_no_val': str})
+
+
+# //////////////////*********************************************/////////////////////************************ #
+
+class CONNECT:
+    DB = "com.tmax.tibero.jdbc.TbDriver"
+    PORT = "jdbc:tibero:thin:@192.169.10.51:18629:DSTFCC"
+    # USER = ["natdilok","Kbjparn#3009"]| #user p parn
+    USER = ["supat", "coll_sp@2025"]
+    CNN = "tibero6-jdbc.jar"
+    
+conn = jaydebeapi.connect(
+        CONNECT.DB,
+        CONNECT.PORT,
+        CONNECT.USER,
+        CONNECT.CNN,
+        )
+cur = conn.cursor()  
+
+
+sms_type = pd.read_sql(f'''
+      SELECT * FROM SIREETRON.REF_SMS_WORDING 
+    ''', conn) 
+conn.close()
+sms_type = clean_column_names(sms_type)
+
+
+assign_delay = assign_delay.merge(sms_type,left_on='sms_type',right_on = 'sms_type', how='left')
+assign_delay['customer_no'] = assign_delay['customer_no'].astype(str)
+assign_delay['contract_no'] = assign_delay['contract_no'].astype(str)
+customer['contract_no_val'] = customer['contract_no_val'].astype(str)
+customer['national_id'] = customer['national_id'].astype(str) 
+
+
+assign_delay['customer_name'] = assign_delay['customer_name'].str.strip()
+assign_delay[['name', 'surname']] = assign_delay['customer_name'].str.split(n=1, expand=True)
+
+
+assign_delay_merge = assign_delay.merge(customer, left_on=['customer_no', 'contract_no'], right_on=['national_id' ,'contract_no_val'], how='left')
+assign_delay_merge['name_check'] = assign_delay_merge.apply(
+    lambda row: True if row['name'] == row['name_val'] else row['name_val'],
     axis=1
 )
-assign_delay_merge['SURNAME_CHECK'] = assign_delay_merge.apply(
-    lambda row: True if row['SURNAME'] == row['SURNAME_VAL'] else row['SURNAME_VAL'],
+
+assign_delay_merge['surname_check'] = assign_delay_merge.apply(
+    lambda row: True if row['surname'] == row['surname_val'] else row['surname_val'],
     axis=1
 )
 
-assign_delay_merge['MOBILE_CHECK'] = assign_delay_merge.apply(
-    lambda row: True if row['Mobile No.'] == row['MOBILE_PHONE_NO_VAL'] else row['MOBILE_PHONE_NO_VAL'],
+assign_delay_merge['mobile_check'] = assign_delay_merge.apply(
+    lambda row: True if row['mobile_no'] == row['mobile_phone_no_val'] else row['mobile_phone_no_val'],
     axis=1
 )
 
-assign_delay_merge['LAST4DIGIT'] = assign_delay_merge['Loan No.'].str[-4:]
-assign_delay_merge = assign_delay_merge[['Base date', 'Customer ID No', 'Principal balance', 'Customer Name',
-       'Sales product code', 'Mobile No.', 'Overdue Months Division Code',
-       'Loan No.', 'Overdue days(Morning)', 'Status LM', 'New OA', 'NAME',
-       'SURNAME', 'AS_OF_DATE', 'CHANNEL',
-       'MONTHLY_INST_AMT', 'FIRST_DUE_DATE','RESIDENT_ADDRESS', 'NAME_CHECK',
-       'SURNAME_CHECK', 'MOBILE_CHECK','LAST4DIGIT']]
-path_file = glob.glob("./input/1stday/*assign*.xlsx")[0].split('\\')[1]
+assign_delay_merge['fullname_check'] = assign_delay_merge.apply(
+    lambda row: row['customer_name'] if row['name'] == row['name_val'] and row['surname'] == row['surname_val']  else row['name_val'] + ' ' +row['surname_val'] ,
+    axis=1
+)
+
+
+assign_delay_merge['last4digit_val'] = assign_delay_merge['contract_no'].str[-4:]
+assign_delay_merge['sms_wording'] = assign_delay_merge.apply(lambda row: row['sms_wording'].replace("{4digit}", str(row['last4digit_val'])), axis=1)
+
+
+path_file = glob.glob("./input/assign_input/*assign*.xlsx")[0].split('\\')[1]
 path_file = re.sub(r'\.xlsx$', '', path_file)
-assign_delay_merge.to_csv(f'./output/1stday/check{path_file}')
+
+assign_delay_merge.to_csv(f'./output/check{path_file}.csv')
+
+
+# ///////////////////////////////////////////////////// ***
+
 
